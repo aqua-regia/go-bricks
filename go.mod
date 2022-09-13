6111c3cec2cda853bb3f09a1aa28e6da0edd40ed
hassan_ashraf_ <hassan@coinswitch.co>
2022-09-13 12:51:04 +0000

    redis based locking implementation redis based ipratelimiter recovery function middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module bitbucket.org/coinswitch/go-bricks
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v9 v9.0.0-beta.2
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ redis_lock/redislock_example_test.go
@@ -0,0 +1,112 @@
+package redis_lock_test
+
+import (
+	"bitbucket.org/coinswitch/go-bricks/redis_lock"
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v9"
+	"log"
+	"time"
+)
+
+func Example() {
+	// Connect to redis.
+	client := redis.NewClient(&redis.Options{
+		Network: "tcp",
+		Addr:    "127.0.0.1:6379",
+	})
+	defer client.Close()
+
+	// Create a new lock client.
+	locker := redis_lock.Client{}
+
+	ctx := context.Background()
+
+	// Try to obtain lock.
+	lock, err := locker.Obtain(ctx, "my-key", 100*time.Millisecond, nil)
+	if err == redis_lock.ErrNotObtained {
+		fmt.Println("Could not obtain lock!")
+	} else if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Don't forget to defer Release.
+	defer lock.Release(ctx)
+	fmt.Println("I have a lock!")
+
+	// Sleep and check the remaining TTL.
+	time.Sleep(50 * time.Millisecond)
+	if ttl, err := lock.TTL(ctx); err != nil {
+		log.Fatalln(err)
+	} else if ttl > 0 {
+		fmt.Println("Yay, I still have my lock!")
+	}
+
+	// Extend my lock.
+	if err := lock.Refresh(ctx, 100*time.Millisecond, nil); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sleep a little longer, then check.
+	time.Sleep(100 * time.Millisecond)
+	if ttl, err := lock.TTL(ctx); err != nil {
+		log.Fatalln(err)
+	} else if ttl == 0 {
+		fmt.Println("Now, my lock has expired!")
+	}
+
+	// Output:
+	// I have a lock!
+	// Yay, I still have my lock!
+	// Now, my lock has expired!
+}
+
+func ExampleClient_Obtain_retry() {
+	client := redis.NewClient(&redis.Options{Network: "tcp", Addr: "127.0.0.1:6379"})
+	defer client.Close()
+
+	locker := redis_lock.New(client)
+
+	ctx := context.Background()
+
+	// Retry every 100ms, for up-to 3x
+	backoff := redis_lock.LimitRetry(redis_lock.LinearBackoff(100*time.Millisecond), 3)
+
+	// Obtain lock with retry
+	lock, err := locker.Obtain(ctx, "my-key", time.Second, &redis_lock.Options{
+		RetryStrategy: backoff,
+	})
+	if err == redis_lock.ErrNotObtained {
+		fmt.Println("Could not obtain lock!")
+	} else if err != nil {
+		log.Fatalln(err)
+	}
+	defer lock.Release(ctx)
+
+	fmt.Println("I have a lock!")
+}
+
+func ExampleClient_Obtain_customDeadline() {
+	client := redis.NewClient(&redis.Options{Network: "tcp", Addr: "127.0.0.1:6379"})
+	defer client.Close()
+
+	locker := redis_lock.New(client)
+
+	// Retry every 500ms, for up-to a minute
+	backoff := redis_lock.LinearBackoff(500 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Minute))
+	defer cancel()
+
+	// Obtain lock with retry + custom deadline
+	lock, err := locker.Obtain(ctx, "my-key", time.Second, &redis_lock.Options{
+		RetryStrategy: backoff,
+	})
+	if err == redis_lock.ErrNotObtained {
+		fmt.Println("Could not obtain lock!")
+	} else if err != nil {
+		log.Fatalln(err)
+	}
+	defer lock.Release(context.Background())
+
+	fmt.Println("I have a lock!")
+}
