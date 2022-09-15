0a3a1dc47c2d668cb6481ddbd84bd5cf2de0fb1b
hassan_ashraf_ <hassan@coinswitch.co>
2022-09-15 14:11:23 +0000

    removed import test

--- import_test.go
+++ import_test.go
@@ -1,40 +1,20 @@
 package go_bricks
 
-import (
-	"fmt"
-	"github.com/Jeffail/gabs/v2"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/go-gormigrate/gormigrate/v2"
-	redisv7 "github.com/go-redis/redis/v7"
-	redisv8 "github.com/go-redis/redis/v8"
-	"github.com/google/uuid"
-	"github.com/newrelic/go-agent/v3/newrelic"
-	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-
-	"github.com/stretchr/testify/assert"
-	"gorm.io/datatypes"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
 func main() {
-	fmt.Println(gabs.ParseJSON([]byte(`{}`)))
-	fmt.Println(aws.String("hello world"))
-	fmt.Println(cors.DefaultSchemas)
-	fmt.Println(gin.ContextKey)
-	fmt.Println(gormigrate.ErrUnknownPastMigration)
-	fmt.Println(redisv8.XAutoClaimCmd{})
-	fmt.Println(redisv7.ClusterSlotsCmd{})
-	fmt.Println(uuid.New().String())
-	fmt.Println(newrelic.Application{})
-	fmt.Println(logrus.DebugLevel)
-	fmt.Println(viper.AllKeys())
-	fmt.Println(assert.CallerInfo())
-	fmt.Println(datatypes.URL{})
-	fmt.Println(postgres.Index{TableName: "hello"})
-	fmt.Println(gorm.DeletedAt{})
-	fmt.Println("hello world")
+	//fmt.Println(gabs.ParseJSON([]byte(`{}`)))
+	//fmt.Println(aws.String("hello world"))
+	//fmt.Println(cors.DefaultSchemas)
+	//fmt.Println(gin.ContextKey)
+	//fmt.Println(gormigrate.ErrUnknownPastMigration)
+	//fmt.Println(redisv8.XAutoClaimCmd{})
+	//fmt.Println(redisv7.ClusterSlotsCmd{})
+	//fmt.Println(uuid.New().String())
+	//fmt.Println(newrelic.Application{})
+	//fmt.Println(logrus.DebugLevel)
+	//fmt.Println(viper.AllKeys())
+	//fmt.Println(assert.CallerInfo())
+	//fmt.Println(datatypes.URL{})
+	//fmt.Println(postgres.Index{TableName: "hello"})
+	//fmt.Println(gorm.DeletedAt{})
+	//fmt.Println("hello world")
 }
